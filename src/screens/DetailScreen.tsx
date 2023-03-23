b0ed4dac2cc3f83dd8d2b115036a42641f37be59
Suleyman Oner <suleymanoner1999@gmail.com>
2023-03-23 23:17:17 +0100

    little changes done

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {
   Text,
   View,
@@ -10,6 +10,15 @@ import {
 } from 'react-native';
 import {ButtonWithText} from '../components/ButtonWithText';
 import {MAIN_COLOR} from '../utils/Config';
+import ImageCropPicker from 'react-native-image-crop-picker';
+import DataContext from '../utils/DataContext';
+
+interface Fox {
+  id: string;
+  name: string;
+  age: string;
+  image: string;
+}
 
 interface DetailScreenProps {
   navigation: any;
@@ -17,26 +26,74 @@ interface DetailScreenProps {
 }
 
 const DetailScreen: React.FC<DetailScreenProps> = ({navigation, route}) => {
-  const {id, name, age, image} = route?.params;
+  const {id, name, age, image, updateFox} = route?.params;
 
-  const [stateName, setStateName] = useState(name);
-  const [stateAge, setStateAge] = useState(age);
-  const [stateImg, setStateImg] = useState(image);
+  var fox: Fox = {
+    id,
+    name,
+    age,
+    image,
+  };
+
+  const [stateName, setStName] = useState(name);
+  const [stateAge, setStAge] = useState(age);
+  const [stateImg, setStImg] = useState(image);
+  const [isPhotoChanged, setIsPhotoChanged] = useState(false);
+
+  const {data, setData} = useContext(DataContext);
+
+  const goBack = () => {
+    navigation.goBack();
+  };
 
   const onChangeVariable = (type: string, newVariable: string) => {
     if (type === 'name') {
-      setStateName(newVariable);
+      setStName(newVariable);
     } else if (type === 'age') {
-      setStateAge(newVariable);
+      setStAge(newVariable);
     }
+  };
+
+  const choosePhoto = () => {
+    try {
+      ImageCropPicker.openPicker({
+        width: Dimensions.get('screen').width / 1.08,
+        height: Dimensions.get('screen').height / 2.5,
+        cropping: true,
+      })
+        .then(img => {
+          setStImg(img.path);
+          console.log(img.path);
+          setIsPhotoChanged(true);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSave = () => {
+    updateFox(id, stateName, stateAge, stateImg);
+    goBack();
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.img_container}>
-        <TouchableOpacity>
+      <View style={styles.top_container}>
+        <TouchableOpacity onPress={goBack}>
           <Image
-            source={require('../assets/foxes/fox0.jpg')}
+            source={require('../assets/left-arrow.png')}
+            style={styles.back_button}
+          />
+        </TouchableOpacity>
+        <Text style={styles.top_container_title}>Edit Details</Text>
+      </View>
+      <View style={styles.img_container}>
+        <TouchableOpacity onPress={choosePhoto}>
+          <Image
+            source={isPhotoChanged ? {uri: stateImg} : stateImg}
             style={styles.image}
           />
         </TouchableOpacity>
@@ -58,7 +115,7 @@ const DetailScreen: React.FC<DetailScreenProps> = ({navigation, route}) => {
         />
       </View>
       <View style={styles.btn_container}>
-        <ButtonWithText title="Save" onTap={() => {}} />
+        <ButtonWithText title="Save" onTap={onSave} />
       </View>
     </View>
   );
@@ -69,9 +126,21 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
+  top_container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  top_container_title: {
+    fontSize: 30,
+    marginTop: 10,
+    color: 'black',
+    textAlign: 'center',
+  },
   img_container: {
     alignItems: 'center',
-    marginTop: 15,
+    marginTop: 10,
   },
   info_container: {
     alignItems: 'flex-start',
@@ -87,24 +156,26 @@ const styles = StyleSheet.create({
     height: Dimensions.get('screen').height / 2.5,
     borderRadius: 5,
   },
-  name: {
-    fontSize: 35,
-    color: MAIN_COLOR,
-    fontWeight: '500',
-  },
   input: {
     borderWidth: 0.8,
     borderRadius: 10,
     borderColor: MAIN_COLOR,
     padding: 10,
     width: Dimensions.get('screen').width / 1.08,
-    fontSize: 30,
+    fontSize: 23,
     color: MAIN_COLOR,
     marginTop: 10,
   },
   headerText: {
-    fontSize: 17,
+    fontSize: 16,
     color: MAIN_COLOR,
+  },
+  back_button: {
+    width: 25,
+    height: 25,
+    right: 75,
+    position: 'absolute',
+    bottom: -20,
   },
 });
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import {Text, View, FlatList} from 'react-native';
-import foxes from '../assets/foxes.json';
-import foxImages from '../assets/foxes';
+import React, {useContext} from 'react';
+import {View, FlatList} from 'react-native';
 import FoxComponent from '../components/FoxComponent';
+import DataContext from '../utils/DataContext';
+
+import FloatingActionButton from '../components/FloatingActionButton';
+
+interface Fox {
+  id: string;
+  name: string;
+  age: string;
+  image: string;
+}
 
 interface HomeScreenProps {
   navigation: any;
 }
 
 const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
+  const {data, setData} = useContext(DataContext);
+
   const goDetails = (id: string, name: string, age: string, image: string) => {
-    navigation.navigate('DetailScreen', {id, name, age, image});
+    navigation.navigate('DetailScreen', {id, name, age, image, updateFox});
+  };
+
+  const goAddNew = () => {
+    navigation.navigate('AddNewFoxScreen');
+  };
+
+  const updateFox = (id: string, name: string, age: string, image: string) => {
+    const updatedData = data.map(fox => {
+      if (fox.id === id) {
+        return {...fox, name, age, image};
+      }
+      return fox;
+    });
+    setData(updatedData);
   };
 
   return (
     <View>
       <FlatList
-        data={foxes}
+        data={data}
         keyExtractor={item => item.id}
         renderItem={({item}) => (
           <FoxComponent
@@ -26,6 +50,7 @@ const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
           />
         )}
       />
+      <FloatingActionButton onPress={() => goAddNew()} />
     </View>
   );
 };
